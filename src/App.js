651ddca207242370127839e53309018044782718
Eason <bettercalleason@gmail.com>
2022-07-26 19:50:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import cat from './imgs/cat.jpeg';
+import catHide from './imgs/cat-hide.jpeg';
+import eye from './imgs/eye.png';
+import eyeSlash from './imgs/eyeSlash.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [isCatHide, setIsCatHide] = useState(false);
+    const [isHide, setIsHide] = useState(true);
+
+    const checkIfHide = (e) => {
+        console.log('hi', e.target.tagName);
+        if (e.target.tagName === 'INPUT' && e.type === 'focus') {
+            setIsCatHide(isHide);
+        } else if (e.target.tagName === 'INPUT' && e.type === 'blur') {
+            setIsCatHide(false);
+        } else if (e.target.tagName === 'DIV') {
+            setIsCatHide(false);
+        } else if (e.target.tagName === 'IMG') {
+            const nowHide = !isHide;
+            setIsCatHide(nowHide);
+            setIsHide(nowHide);
+        }
+    };
+    return (
+        <div
+            className="App"
+            style={{ width: '100%', height: '100vh' }}
+            onClick={checkIfHide}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <img src={isCatHide ? catHide : cat} style={{ height: '100px' }} />
+            <div>
+                <div>
+                    <input type="text" />
+                </div>
+                <div
+                    style={{
+                        position: 'relative',
+                        width: 'fit-content',
+                        margin: 'auto',
+                    }}
+                >
+                    <input
+                        type={isHide ? 'password' : 'text'}
+                        onFocus={checkIfHide}
+                        onBlur={checkIfHide}
+                    />
+                    <div
+                        style={{ position: 'absolute', right: '0', top: '0' }}
+                        onClick={checkIfHide}
+                    >
+                        <img
+                            src={isHide ? eyeSlash : eye}
+                            style={{ width: '30px' }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
